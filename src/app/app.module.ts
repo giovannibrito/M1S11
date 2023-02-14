ea547692418ca9a06b1891fc7723512240344aee
giovannibrito <giovannileonibrito@bol.com.br>
2023-02-14 16:05:35 -0300

    Adiciona ex 4

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { CaptalizePhilipsPipe } from './pipes/captalize-philips.pipe';
 import { Ex1Component } from './components/ex1/ex1.component';
 import { Ex2Component } from './components/ex2/ex2.component';
 import { Ex3Component } from './components/ex3/ex3.component';
+import { Ex4Component } from './components/ex4/ex4.component';
 
 @NgModule({
   declarations: [
@@ -13,7 +14,8 @@ import { Ex3Component } from './components/ex3/ex3.component';
     CaptalizePhilipsPipe,
     Ex1Component,
     Ex2Component,
-    Ex3Component
+    Ex3Component,
+    Ex4Component
   ],
   imports: [
     BrowserModule
