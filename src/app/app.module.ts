b5f54d83f29ce35980d1c0ca0e88acf68f035862
giovannibrito <giovannileonibrito@bol.com.br>
2023-02-14 15:48:16 -0300

    Adiciona ex 2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { CaptalizePhilipsPipe } from './pipes/captalize-philips.pipe';
+import { Ex1Component } from './components/ex1/ex1.component';
+import { Ex2Component } from './components/ex2/ex2.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CaptalizePhilipsPipe
+    CaptalizePhilipsPipe,
+    Ex1Component,
+    Ex2Component
   ],
   imports: [
     BrowserModule
